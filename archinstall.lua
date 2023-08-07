e91de207f2e9312b24cdc87f16d0a873c253334e
root <pizuhh@gmail.com>
2023-08-07 12:52:38 +0000

    updated

--- archinstall.lua
+++ archinstall.lua
@@ -1,7 +1,82 @@
+function setup_grub()
+	print("Do you even want to install GRUB? If you don't want to install grub it'll make this system unbootable until you add it to your current bootloader (if you dualboot)[y/n]")
+	local yes_no = io.read()
+	if yes_no == "y" then
+		--installing grub
+
+		print("pacman -S grub os-prober")
+		os.execute("pacman -S grub os-prober")
+		io.write("Where are we installing grub? (should be on the disk you are installing the system, example /dev/sda): ")
+		local disk = io.read()
+		os.execute(string.format("grub-install --target=i386-pc %s", disk))
+		os.execute("grub-mkconfig -o /boot/grub/grub.cfg")
+	else 
+		print("You can always install grub or other bootloader later")
+		os.execute("sleep 5")
+		chroot_setup()
+	
+	end
+end
+
+
+function usr_setup()
+	-- user setup
+	io.write("User name: ")
+	local username = io.read()
+
+	os.execute("echo %wheel ALL=(ALL:ALL) ALL >> /etc/sudoers")
+	os.execute(string.format("useradd -m %s", username))
+	os.execute(string.format("usermod -aG wheel,audio,video,storage %s", usernaame))
+	
+	print("Setting up password for your user\n\n")
+	os.execute(string.format("passwd %s", username))
+	print("Setting up password for root user\n\n")
+	os.execute("passwd")
+	
+	-- hostname
+	io.write("Enter hostname (example arch): ")
+	local hostname = io.read()
+	os.execute(string.format("echo %s >> /etc/hostname", hostname))
+	
+	-- locale; setting up the default one for now
+	local locale = "en_US.UTF-8"
+	os.execute(string.format("echo %s >> /etc/locale.gen", locale))
+	os.execute(string.format("echo LANG=%s >> /etc/locale.conf", locale))
+	os.execute(string.format("export LANG=%s", locale))
+
+
+end
+
+
+function chroot_setup()
+	os.execute("clear")
+
+	print("1. Install few packages(networkmanager, sudo, base-devel)\n2. Set-up loacels, users, hostname and sudo\n3. Install boot manager (GRUB)\n4. Choose desktop\n5. Exit chroot")
+	local choice = io.read()
+	if choise == "1" then
+		os.execute("pacman -S networkmanager sudo base-devel")
+		chroot_setup()
+	elseif choice == "2" then
+		usr_setup()
+		chroot_setup()
+	elseif choice == "3" then
+		setup_grub()
+		chroot_setup()
+	elseif choice == "4" then
+		DE()
+		chroot_setup()
+	elseif choice == "5" then
+		os.execute("exit")
+
+	end
+
+end
+
+
 function install()
 	io.write("Pacstrap will install the base system! the default command will be: \"pacstrap -K /mnt linux linux-firmware base vim lua\" (if you want to install other packages enter the names here MAKE SURE THEY ARE IN CORE AND EXTRA REPOS! or type none to skip this): ")
 	pkg = io.read()
-	local command = "pacstrap -K /mnt linux linux-firmware base vim lua"
+	local command = "pacstrap -K /mnt linux linux-firmware base vim lua git"
 	if pkg ~= "none" then
 		local full = string.format("%s %s", command, pkg)
 		print(full)
@@ -43,7 +118,7 @@ end
 
 function main()
 	os.execute("clear")
-	print("1. Partition the disk\n2. Format and mount the paritions\n3. Pacstrap the system and generate fstab\n4. CHROOT")
+	print("1. Partition the disk\n2. Format and mount the paritions\n3. Pacstrap the system and generate fstab\n4. CHROOT\n5.CHROOT-SETUP(execute only when you are in chroot environment!) ")
 	local choice = io.read()
 	if choice == "1" then
 		os.execute("cfdisk")
@@ -71,7 +146,10 @@ function main()
 		os.execute("cp ./archinstall.lua /mnt/archinstall.lua")
 		os.execute("arch-chroot /mnt")
 
-	
+	elseif choice == "5" then
+		chroot_setup()
+		main()
+
 	else 
 		print("Invalid option!")
 	end
